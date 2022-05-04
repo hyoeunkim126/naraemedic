8b14083052d17aafc36b4963bc8fe3cd3d4ab278
hyoeunkim126 <hyoeun126@naver.com>
2022-05-04 15:50:13 +0900

    0504

--- js/script.js
+++ js/script.js
@@ -15,3 +15,11 @@ $(function(){
 
 })
 
+$(function(){
+    $('.mainmenu>li').mouseenter(function(){
+        $(this).find('.submenu').stop().slideDown();
+    });
+    $('.mainmenu>li').mouseleave(function(){
+        $(this).find('.submenu').stop().slideUp();
+    });
+});
